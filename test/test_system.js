3e75e26c462465f33666722f8a138b6a740b7f04
McDonald, Ryan <Ryan.McDonald@nordstrom.com>
2018-06-29 16:36:00 -0700

    tested System#removeBody()

--- test/test_system.js
+++ test/test_system.js
@@ -14,4 +14,14 @@ describe("System", () => {
 		let found = system.getBodyById(body.id);
 		assert.equal(found.id, body.id, "should find same body");
 	});
+
+	it("should allow bodies to be removed", () => {
+		let system = new lib.System();
+		let body   = new lib.Body();
+		system.addBody(body);
+		assert.equal(system.bodies.length, 1, "should have 1 body");
+
+		system.removeBody(body);
+		assert.equal(system.bodies.length, 0, "should have 0 bodies");
+	});
 });
