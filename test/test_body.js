a85433b72cb5546d2bb52f406ed99a06e95126d7
McDonald, Ryan <Ryan.McDonald@nordstrom.com>
2018-07-05 13:21:14 -0700

    using threejs objects for vectors & meshes with position

--- test/test_body.js
+++ test/test_body.js
@@ -1,24 +1,30 @@
 
 const assert = require('assert');
 const lib    = require('../lib');
+const three  = require('three');
 
 describe("Body", () => {
 	it("should get default values", () => {
+
+		let geometry = new three.BoxBufferGeometry(1,1,1);
+		let material = new three.MeshBasicMaterial({color:0xffff00});
+		let mesh     = new three.Mesh(geometry, material);
+
 		for (let i=0; i<100; i++) {
-			let body = new lib.Body();
+			let body = new lib.Body(mesh);
 			assert.equal(body.id.length, 36, "id should be 16 characters");
 
-			assert.equal(body.velocity.x, 0, "should have zero velocity");
-			assert.equal(body.velocity.y, 0, "should have zero velocity");
-			assert.equal(body.velocity.z, 0, "should have zero velocity");
+			assert.equal(body.position.x, 0, "should have position = 0");
+			assert.equal(body.position.y, 0, "should have position = 0");
+			assert.equal(body.position.z, 0, "should have position = 0");
 
-			assert(body.position.x >= 0, "should have position >= 0");
-			assert(body.position.y >= 0, "should have position >= 0");
-			assert(body.position.z >= 0, "should have position >= 0");
+			assert.equal(body.velocity.x, 0, "should have zero = 0");
+			assert.equal(body.velocity.y, 0, "should have zero = 0");
+			assert.equal(body.velocity.z, 0, "should have zero = 0");
 
-			assert(body.position.x <= 100, "should have position <= 100");
-			assert(body.position.y <= 100, "should have position <= 100");
-			assert(body.position.z <= 100, "should have position <= 100");
+			assert.equal(body.spin.x, 0, "should have spin = 100");
+			assert.equal(body.spin.y, 0, "should have spin = 100");
+			assert.equal(body.spin.z, 0, "should have spin = 100");
 		}
 	});
 });
--- test/test_vec3.js
+++ test/test_vec3.js
@@ -1,42 +1,21 @@
 
 const assert = require('assert');
-const Vec3   = require('../lib/vec3.js').Vec3;
+const vec3   = require('../lib/vec3.js');
 
 describe("Vec3", () => {
 
-	it("should have default values", () => {
-		let v = new Vec3();
-		assert.equal(v.x, 0, "should be zero");
-		assert.equal(v.y, 0, "should be zero");
-		assert.equal(v.z, 0, "should be zero");
-	});
-
-	it("should allow changes", () => {
-		let v = new Vec3();
-		assert.equal(v.x, 0, "should be zero");
-		v.x = 1;
-		assert.equal(v.x, 1, "should be one");
-	});
-
 	it("should have a custom toString()", () => {
-		let v = new Vec3();
+		let v = vec3.new();
 		assert.equal(v.toString(), "{0,0,0}", "should just print values in braces");
 	});
 
-	it("should have a distance()", () => {
-		let v1 = new Vec3(1,2,3);
-		let v2 = new Vec3(11,2,3);
-		assert.equal(v1.distance(v2), v2.distance(v1), "distances should be the same");
-		assert.equal(v1.distance(v2), 10, "distance should be 10");
-	});
-
-	it("should return random values for Vec3.random()", () => {
+	it("should return random values in expected bounds for vec3.random()", () => {
 		let max       = 5;
 		let count     = 5000;
 		let foundMax  = false;
 		let foundZero = false;
 		for (let i=0; i<count; i++) {
-			let v = Vec3.random(max);
+			let v = vec3.random(0, max);
 			assert(v.x<=max, "should be less than or equal to provided max: "+ max);
 			assert(v.x>=0,   "should be equal to or greater than zero");
 			assert(v.y<=max, "should be less than or equal to provided max: "+ max);
